67329e82764171e9e029c096650f59b1db745db6
Miklós Márton <martonmiklosqdev@gmail.com>
2022-02-05 16:29:30 +0100

    Sequencing works

--- sw/brute_force.py
+++ sw/brute_force.py
@@ -14,7 +14,9 @@ class Button(IntEnum):
     Increase = 4
     Fertig = 14
 
-PowerEn = 15
+PowerEn = 18
+DockPowerEn = 15
+
 try:
     from PIL import Image
 except ImportError:
@@ -22,10 +24,13 @@ except ImportError:
 import pytesseract
 
 videodev = '/dev/video0'
-ROI = (605, 150, 605+289, 150+80)
+ROI = (0, 0, 1280, 720)
+ROI = (246, 237, 890, 394)
+
 
 pinlist = []
 power = False
+dockPower = False
 
 def gpio_init() :
     GPIO.setmode(GPIO.BCM)
@@ -33,7 +38,9 @@ def gpio_init() :
     GPIO.setup(4, GPIO.OUT)
     GPIO.setup(14, GPIO.OUT)
     GPIO.setup(PowerEn, GPIO.OUT)
-
+    GPIO.setup(DockPowerEn, GPIO.OUT)
+    GPIO.output(PowerEn, GPIO.LOW)
+    GPIO.output(DockPowerEn, GPIO.LOW)
 
 def set_power_state(power_on):
     global power
@@ -45,19 +52,29 @@ def set_power_state(power_on):
         print("Power OFF")
         GPIO.output(PowerEn, GPIO.LOW)
 
+def set_dock_power_state(power_on):
+    global dockPower
+    dockPower = power_on
+    if (power_on == True):
+        print("Dock Power ON")
+        GPIO.output(DockPowerEn, GPIO.HIGH)
+    else:
+        print("Dock Power OFF")
+        GPIO.output(DockPowerEn, GPIO.LOW)
+
 def press_button(button):
     print("Press button: ", button)
     GPIO.output(int(button), GPIO.HIGH)
     time.sleep(0.2)
     GPIO.output(int(button), GPIO.LOW)
 
-def take_image_and_ocr():
+def take_image_and_ocr(savename):
     camera = Camera(videodev, 1280, 720)
     frame = camera.get_frame()
     camera.close()
     image = Image.frombytes('RGB', (camera.width, camera.height), frame, 'raw', 'RGB')
     image = image.crop(ROI)
-    image.save("out.png")
+    image.save(str(savename) + ".png")
     ret = pytesseract.image_to_string(image, 'deu')
     print(ret)
     return ret
@@ -74,9 +91,9 @@ def enter_number(num):
         print("Entering digit: ", digit)
         for step in range(digit):
             press_button(Button.Increase)
-            time.sleep(0.01)
+            time.sleep(0.1)
         press_button(Button.NextDigit) # jump to next digit
-        time.sleep(0.01)
+        time.sleep(0.1)
     press_button(Button.Fertig)
 
 def dictionary_init(startPIN) :
@@ -97,29 +114,46 @@ def dictionary_init(startPIN) :
 def do_bruteforce() :
     pin_index = 0
     while pin_index < len(pinlist):
+        set_dock_power_state(True)
+        time.sleep(5) # delay between the dock and power
         set_power_state(True)
-        #time.sleep(5) # boot time at the beginning
+        time.sleep(32) # boot time at the beginning
         press_button(Button.Fertig) # Fertig
+        time.sleep(0.5)
         pinfound = False
         for retry in range(3):
             enter_number(pinlist[pin_index])
-            ocr = take_image_and_ocr()
-            if not 'Fehler' in ocr and not 'Tasten' in ocr:
+            time.sleep(1)
+            ocr = take_image_and_ocr(pinlist[pin_index]).lower()
+            if not 'fehler' in ocr \
+                    and not 'tasten' in ocr \
+                    and not 'sind' in ocr \
+                    and not 'gespert' in ocr \
+                    and not 'bitte' in ocr \
+                    and not 'kontaktieren' in ocr \
+                    and not 'service' in ocr:
                 pinfound = True
                 break
             pin_index = pin_index + 1
             press_button(Button.Fertig)
-            time.sleep(2)
+            time.sleep(1)
         if pinfound:
             print("Pin found? ", pin_index)
             break
         set_power_state(False)
+        time.sleep(3)
+        set_dock_power_state(False)
+        time.sleep(5)
 
 def button_test():
+    global power
+    global dockPower
     while True:
         c = readchar.readchar()
         if c == 'p':
             set_power_state(not power)
+        elif c == 'd':
+            set_dock_power_state(not dockPower)
         elif c == 'f':
             press_button(Button.Fertig)
         elif c == 'q':
@@ -143,7 +177,7 @@ if __name__ == '__main__':
             gpio_init()
             button_test()
         elif (sys.argv[1] == 'take_image'):
-            take_image_and_ocr()
+            take_image_and_ocr("test")
         else:
             print("Usage:")
             print(" No arguments: start brute force")
@@ -155,4 +189,5 @@ if __name__ == '__main__':
             print("              - +: increase digit")
             print("              - n: next digit")
             print("              - p: toggle power")
+            print("              - d: toggle dock power")
             print(" take_image:  Take a test image and write it to test.png")
